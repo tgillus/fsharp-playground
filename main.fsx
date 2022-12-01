f2a822321cd29a1996eb5e213a705fc8c8c3cba5
Tramaine Gillus <1573935+tgillus@users.noreply.github.com>
2022-12-01 00:45:17 -0500

    feat: complete nth and length

--- main.fsx
+++ main.fsx
@@ -1,20 +1,47 @@
-let rec last list =
-    match list with
-    | [] -> None
-    | [x] -> Some x
-    | _::rest -> last rest
+let rec last = function
+  | [] -> None
+  | [x] -> Some x
+  | _::rest -> last rest
 
-let rec lastTwo list =
-    match list with
-    | [] | [_] -> None
-    | [x; y] -> Some (x, y)
-    | _::rest -> lastTwo rest
+printfn "%b" (last [] = None)
+printfn "%b" (last [1] = Some 1)
+printfn "%b" (last [1; 2] = Some 2)
+printfn "%b" (last [1; 2; 3] = Some 3)
 
-printfn "%A" (last [])
-printfn "%A" (last [1])
-printfn "%A" (last [1; 2])
-printfn "%A" (last [1; 2; 3])
-printfn "%A" (lastTwo [])
-printfn "%A" (lastTwo [1])
-printfn "%A" (lastTwo [1; 2])
-printfn "%A" (lastTwo [1; 2; 3])
+let rec lastTwo = function
+  | [] | [_] -> None
+  | [x; y] -> Some (x, y)
+  | _::rest -> lastTwo rest
+
+printfn "%b" (lastTwo [] = None)
+printfn "%b" (lastTwo [1] = None)
+printfn "%b" (lastTwo [1; 2] = Some (1, 2))
+printfn "%b" (lastTwo [1; 2; 3] = Some (2, 3))
+
+let rec at list n = 
+  match (list, n) with
+    | ([], _) -> None
+    | (first::_, 0) -> Some first
+    | (_::rest, _) -> at rest (n - 1)
+
+printfn "%b" (at [] 0 = None)
+printfn "%b" (at [1] 0 = Some 1)
+printfn "%b" (at [1] 1 = None)
+printfn "%b" (at [1; 2] 0 = Some 1)
+printfn "%b" (at [1; 2] 1 = Some 2)
+printfn "%b" (at [1; 2] 2 = None)
+printfn "%b" (at [1; 2; 3] 0 = Some 1)
+printfn "%b" (at [1; 2; 3] 1 = Some 2)
+printfn "%b" (at [1; 2; 3] 2 = Some 3)
+printfn "%b" (at [1; 2; 3] 3 = None)
+
+let length list =
+  let rec count acc = function 
+    | [] -> acc
+    | _::rest -> count (acc + 1) rest
+  in count 0 list
+
+printfn "%b" (length [] = 0)
+printfn "%b" (length [1] = 1)
+printfn "%b" (length [1; 2] = 2)
+printfn "%b" (length [1; 2; 3] = 3)
